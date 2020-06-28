96a1901daf578744f181e03a5c4c2c813e8f85a0
張 建文 <chang180@gmail.com>
2020-06-28 19:28:29 +0800

    萬事起頭難

--- backend/index.php
+++ backend/index.php
@@ -1,5 +1,10 @@
+<?php include_once "../base.php";
+// if(empty($_SESSION['login'])) to("../index.php");
+?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,7 +12,70 @@
     <link rel="shortcut icon" href="#" type="image/x-icon">
     <title>履歷表後台管理</title>
 </head>
+<link href="https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+<style>
+    * {
+        font-family: 'Noto Sans TC', sans-serif;
+    }
+
+    .main {
+        min-height: 60vh;
+        background: #eee;
+    }
+</style>
+
 <body>
-<h1>履歷表管理後台</h1>
+    <header class="row">
+        <h1 class="col-6">履歷表管理後台</h1>
+        <form class="col-6" action="../api/logout.php">
+            <input type="submit" class="btn btn-outline-primary float-right" value="登出">
+        </form>
+    </header>
+    <div class="container m-auto">
+        <div class="row border">
+            <div class="nav flex-column nav-pills col-3" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+                <a class="btn btn-outline-success m-1 nav-link active" id="title-tab" data-toggle="pill" href="#title" role="tab">個人資料</a>
+                <a class="btn btn-outline-success m-1 nav-link" id="experience-tab" data-toggle="pill" href="#experience" role="tab">學經歷</a>
+                <a class="btn btn-outline-success m-1 nav-link" id="v-pills-messages-tab" data-toggle="pill" href="#v-pills-messages" role="tab">希望職務條件</a>
+                <a class="btn btn-outline-success m-1 nav-link" id="v-pills-settings-tab" data-toggle="pill" href="#v-pills-settings" role="tab">技能</a>
+                <a class="btn btn-outline-success m-1 nav-link" id="v-pills-settings1-tab" data-toggle="pill" href="#v-pills-settings1" role="tab">自傳</a>
+                <a class="btn btn-outline-success m-1 nav-link" id="portfolio-tab" data-toggle="pill" href="#portfolio" role="tab">作品集</a>
+                <a class="btn btn-outline-success m-1 nav-link" id="v-pills-settings3-tab" data-toggle="pill" href="#v-pills-settings3" role="tab">Settings</a>
+                <a class="btn btn-outline-success m-1 nav-link" id="v-pills-settings4-tab" data-toggle="pill" href="#v-pills-settings4" role="tab">Settings</a>
+            </div>
+            <div class="tab-content col-9" id="v-pills-tabContent">
+                <div class="tab-pane fade show active" id="title" role="tabpanel">
+
+                    <div class="row">
+                        <div class="card" style="width: 18rem;">
+                            <!-- <img src="..." class="card-img-top" alt="..."> -->
+                            <div class="card-body">
+                                <h5 class="card-title">張建文</h5>
+                                <p class="card-text">網頁程式設計學習中</p>
+                                <a href="#" class="btn btn-primary">聯絡方式</a>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+                <div class="tab-pane fade" id="experience" role="tabpanel">222</div>
+                <div class="tab-pane fade" id="v-pills-messages" role="tabpanel">333</div>
+                <div class="tab-pane fade" id="v-pills-settings" role="tabpanel">444</div>
+                <div class="tab-pane fade" id="v-pills-settings1" role="tabpanel">555</div>
+                <div class="tab-pane fade" id="portfolio" role="tabpanel">
+                    <a href="http://220.128.133.15/s1090215/">連結</a>
+                </div>
+                <div class="tab-pane fade" id="v-pills-settings3" role="tabpanel">777</div>
+                <div class="tab-pane fade" id="v-pills-settings4" role="tabpanel">888</div>
+            </div>
+        </div>
+    </div>
+
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
 </body>
+
 </html>
---
+++ index1.php
@@ -0,0 +1,183 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="shortcut icon" href="#" type="image/x-icon">
+    <title>我的電子履歷表</title>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <style>
+        * {
+            font-family: 'Noto Sans TC', sans-serif;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="container-main container m-auto">
+        <div class="card portrait" style="width: 18rem;">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+
+        <div id="alerts"></div>
+        <div id="global-status-container">
+            <div id="global-status"></div>
+        </div>
+        <header class="header-item a4-width">
+            <div class="row">
+                <div class="col-xs-8">
+                    <div class="navs"><a class="text-muted active" href="/chang180?ref=resume-header-about">關於</a><a class="text-muted " href="/chang180/portfolio?ref=resume-header-portfolio">作品集</a></div>
+                </div>
+                <div class="col-xs-4 text-right header-toolbar"><a class="text-muted" data-no-turbolink="true" href="https://www.cakeresume.com/messages/chang180"><i class="fa fa-comment-dots"></i></a> <i class="fa fa-print text-muted" onclick="print();"></i></div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <hr />
+                </div>
+            </div>
+        </header>
+        <div class="item-page">
+            <div class="contentarea normal" id="item" notranslate="">
+                <div class="row snippet-profile-021">
+                    <div class="col-xs-12 text-center"><img data-no-retina="true" class="rounded-circle" src="https://media.cakeresume.com/image/upload/s--18zOkBbE--/c_fill,g_face,h_300,w_300/v1592697147/jmyf17ojubo2thr4bgle.jpg" alt="Jmyf17ojubo2thr4bgle" loading="auto">
+                        <h1><b>張建文</b></h1>
+                        <div class="edit">
+                            <div class="social-links"><a href="https://github.com/chang180" target="_blank" rel="nofollow noopener noreferrer"><i class="fab"></i></a>&nbsp;&nbsp;&nbsp;<a href="https://wordpress.com/view/unclelearnsjava.wordpress.com" target="_blank" rel="nofollow noopener noreferrer"><i class="fas"></i></a>&nbsp;&nbsp;&nbsp;<a href="https://www.facebook.com/" target="_blank" rel="nofollow noopener noreferrer"><i class="fab"></i></a>&nbsp;&nbsp;&nbsp;<a href="https://twitter.com/" target="_blank" rel="nofollow noopener noreferrer"><i class="fab"></i></a>&nbsp;</div>
+                        </div>
+                        <div class="row intro">
+                            <div class="col-xs-12 col-md-10 col-md-offset-1">
+                                <h3>網頁程式設計師&nbsp;&nbsp;•&nbsp;&nbsp;Taipei Special Municipality，TW&nbsp;&nbsp;•&nbsp;&nbsp;<a href="/cdn-cgi/l/email-protection#f89b9099969fc9c0c8b89f95999194d69b9795" target="_blank" rel="nofollow noopener noreferrer"><span class="__cf_email__" data-cfemail="97f4fff6f9f0a6afa7d7f0faf6fefbb9f4f8fa">[email&#160;protected]</span></a></h3>
+                                <p>目前在勞動部勞動力發展署泰山職訓場<br>進行PHP資料庫網頁程式設計職前訓練</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row snippet-features-002">
+                    <div class="col-md-12">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <h1><b>技能</b></h1>
+                            </div>
+                        </div>
+                        <div class="row" title="">
+                            <div class="col-md-6 item">
+                                <hr>
+                                <h1>PHP</h1>
+                                <p>網頁設計後端程式語言</p>
+                            </div>
+                            <div class="col-md-6 item">
+                                <hr>
+                                <h1>JavaScript</h1>
+                                <p>前端程式語言</p>
+                            </div>
+                        </div>
+                        <div class="row" title="" style="display: block;">
+                            <div class="col-md-6 item">
+                                <hr>
+                                <h1>MySQL</h1>
+                                <p>MariaDB 資料庫</p>
+                            </div>
+                            <div class="col-md-6 item">
+                                <hr>
+                                <h1>Bootstrap</h1>
+                                <p>前端網頁框架</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row snippet-experiences-014">
+                    <div class="col-xs-12">
+                        <div class="row">
+                            <div class="col-xs-12">
+                                <h1><b>工作經歷</b></h1>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-xs-12 col-md-6">
+                                <div class="row" title="">
+                                    <div class="col-xs-2 col-md-1 item-bullet"></div>
+                                    <div class="col-xs-8 col-md-9 item">
+                                        <h3>勞動力發展署泰山職訓場, 職訓學員, Mar 2020 ~ 現在</h3>
+                                        <p>進行PHP資料庫網頁程式設計職前訓練。</p>
+                                    </div>
+                                </div>
+                                <div class="row" title="">
+                                    <div class="col-xs-2 col-md-1 item-bullet"></div>
+                                    <div class="col-xs-8 col-md-9 item">
+                                        <h3>岱原有限公司, 駐外人員, Feb 2019 ~ Dec 2019</h3>
+                                        <p>負責倉儲管理，運輸工具安排。<br>管理相關外籍工作人員。</p>
+                                    </div>
+                                </div>
+                                <div class="row" title="">
+                                    <div class="col-xs-2 col-md-1 item-bullet"></div>
+                                    <div class="col-xs-8 col-md-9 item">
+                                        <h3>年興紡織, 駐外專員, May 2016 ~ Dec 2018</h3>
+                                        <p>行政，報關，翻譯，現場操作及國外廠商之業務聯絡往來</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-xs-12 col-md-6">
+                                <div class="row" title="">
+                                    <div class="col-xs-2 col-md-1 item-bullet"></div>
+                                    <div class="col-xs-8 col-md-9 item">
+                                        <h3>東森房屋, 營業員, Mar 2011 ~ Feb 2016</h3>
+                                        <p>負責房地產案件之開發及銷售。</p>
+                                    </div>
+                                </div>
+                                <div class="row" title="">
+                                    <div class="col-xs-2 col-md-1 item-bullet"></div>
+                                    <div class="col-xs-8 col-md-9 item">
+                                        <h3>三顧茅蘆滷味, 加盟店長, May 2007 ~ Feb 2011</h3>
+                                        <p>擔任店面管理及面對顧客之銷售工作。</p>
+                                    </div>
+                                </div>
+                                <div class="row" title="">
+                                    <div class="col-xs-2 col-md-1 item-bullet"></div>
+                                    <div class="col-xs-8 col-md-9 item">
+                                        <h3>海軍, 基層軍官, Nov 1997 ~ Jan 2007</h3>
+                                        <p>1.擔任基層幹部。<br>2.曾赴美受訓，返國後曾擔任技術學校教官<br>3.曾擔任部門主管，管理及訓練分發至負責部門之官員、士官及士兵，同時協助主官推動單位之各項行政業務.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row snippet-educations-013 snippet-experiences-013">
+                    <div class="col-xs-12">
+                        <div class="row">
+                            <div class="col-xs-12">
+                                <h1><b>學歷</b></h1>
+                            </div>
+                        </div>
+                        <div class="row" title="">
+                            <div class="col-xs-2 col-md-1 item-bullet"></div>
+                            <div class="col-xs-10 col-md-11 item">
+                                <h3>海軍軍官學校, 學士學位, 企業管理, 1993 ~ 1997</h3>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row snippet-projects-001">
+                    <div class="col-xs-12">
+                        <div class="row">
+                            <div class="col-xs-12">
+                                <h1><b>專案</b></h1>
+                            </div>
+                        </div>
+
+
+
+                        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+                        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+                        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+</body>
+
+</html>
