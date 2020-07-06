77ffddf3f046a6619c3598b3d1431a72d7b64d37
chang180 <chang180@gmail.com>
2020-07-06 13:04:26 +0800

    bug fix

--- backend/index.php
+++ backend/index.php
@@ -41,8 +41,8 @@
                 <a class="btn btn-outline-success m-1 nav-link <?= (@$_COOKIE['do'] == 'experience') ? "active" : ""; ?>" id="experience-tab" data-toggle="pill" href="#experience" role="tab">經歷</a>
                 <a class="btn btn-outline-success m-1 nav-link <?= (@$_COOKIE['do'] == 'jobcondition') ? "active" : ""; ?>" id="jobcondition-tab" data-toggle="pill" href="#jobcondition" role="tab">希望職務條件</a>
                 <a class="btn btn-outline-success m-1 nav-link <?= (@$_COOKIE['do'] == 'skill') ? "active" : ""; ?>" id="skill-tab" data-toggle="pill" href="#skill" role="tab">技能</a>
-                <a class="btn btn-outline-success m-1 nav-link <?= (@$_COOKIE['do'] == 'autobiography') ? "active" : ""; ?>" " id=" autobiography-tab" data-toggle="pill" href="#autobiography" role="tab">自傳</a>
-                <a class="btn btn-outline-success m-1 nav-link" id="portfolio-tab" data-toggle="pill" href="#portfolio" role="tab">作品集</a>
+                <a class="btn btn-outline-success m-1 nav-link <?= (@$_COOKIE['do'] == 'autobiography') ? "active" : ""; ?>" id=" autobiography-tab" data-toggle="pill" href="#autobiography" role="tab">自傳</a>
+                <a class="btn btn-outline-success m-1 nav-link <?= (@$_COOKIE['do'] == 'portfolio') ? "active" : ""; ?>" id="portfolio-tab" data-toggle="pill" href="#portfolio" role="tab">作品集</a>
                 <a class="btn btn-outline-success m-1 nav-link" id="seventh-tab" data-toggle="pill" href="#seventh" role="tab">Settings</a>
                 <a class="btn btn-outline-success m-1 nav-link" id="eighth-tab" data-toggle="pill" href="#eighth" role="tab">Settings</a>
             </div>
@@ -71,7 +71,7 @@
                 <div class="tab-pane fade  <?= (@$_COOKIE['do'] == 'autobiography') ? "show active" : ""; ?>" id="autobiography" role="tabpanel">
                     <?php include "autobiography.php"; ?>
                 </div>
-                <div class="tab-pane fade" id="portfolio" role="tabpanel">
+                <div class="tab-pane fade <?= (@$_COOKIE['do'] == 'portfolio') ? "show active" : ""; ?>" id="portfolio" role="tabpanel">
                     <a href="http://220.128.133.15/s1090215/">連結</a>
                 </div>
                 <div class="tab-pane fade" id="seventh" role="tabpanel">預留兩欄</div>
--- index.php
+++ index.php
@@ -47,8 +47,8 @@ if (!empty($_POST['acc']) && !empty($_POST['pw'])) {
                 <a class="btn btn-outline-success m-1 nav-link <?= (@$_COOKIE['do'] == 'experience') ? "active" : ""; ?>" id="experience-tab" data-toggle="pill" href="#experience" role="tab">經歷</a>
                 <a class="btn btn-outline-success m-1 nav-link <?= (@$_COOKIE['do'] == 'jobcondition') ? "active" : ""; ?>" id="jobcondition-tab" data-toggle="pill" href="#jobcondition" role="tab">希望職務條件</a>
                 <a class="btn btn-outline-success m-1 nav-link <?= (@$_COOKIE['do'] == 'skill') ? "active" : ""; ?>" id="skill-tab" data-toggle="pill" href="#skill" role="tab">技能</a>
-                <a class="btn btn-outline-success m-1 nav-link <?= (@$_COOKIE['do'] == 'autobiography') ? "active" : ""; ?>" " id=" autobiography-tab" data-toggle="pill" href="#autobiography" role="tab">自傳</a>
-                <a class="btn btn-outline-success m-1 nav-link" id="portfolio-tab" data-toggle="pill" href="#portfolio" role="tab">作品集</a>
+                <a class="btn btn-outline-success m-1 nav-link <?= (@$_COOKIE['do'] == 'autobiography') ? "active" : ""; ?>" id=" autobiography-tab" data-toggle="pill" href="#autobiography" role="tab">自傳</a>
+                <a class="btn btn-outline-success m-1 nav-link <?= (@$_COOKIE['do'] == 'portfolio') ? "active" : ""; ?>" id="portfolio-tab" data-toggle="pill" href="#portfolio" role="tab">作品集</a>
                 <a class="btn btn-outline-success m-1 nav-link" id="seventh-tab" data-toggle="pill" href="#seventh" role="tab">Settings</a>
                 <a class="btn btn-outline-success m-1 nav-link" id="eighth-tab" data-toggle="pill" href="#eighth" role="tab">Settings</a>
             </div>
@@ -77,7 +77,7 @@ if (!empty($_POST['acc']) && !empty($_POST['pw'])) {
                 <div class="tab-pane fade  <?= (@$_COOKIE['do'] == 'autobiography') ? "show active" : ""; ?>" id="autobiography" role="tabpanel">
                     <?php include "./front/autobiography.php"; ?>
                 </div>
-                <div class="tab-pane fade" id="portfolio" role="tabpanel">
+                <div class="tab-pane fade <?= (@$_COOKIE['do'] == 'portfolio') ? "show active" : ""; ?>" id="portfolio" role="tabpanel">
                     <a href="http://220.128.133.15/s1090215/">連結</a>
                 </div>
                 <div class="tab-pane fade" id="seventh" role="tabpanel">預留兩欄</div>
